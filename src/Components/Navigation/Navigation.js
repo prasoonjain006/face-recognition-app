a7e1b6fae1320081b5ce955fe1372e37bddaa8cb
prasoonjain006 <prasoonjain006@gmail.com>
2020-06-06 06:56:06 +0530

    update package

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const navigation=()=>{
+    return(
+        <nav style={{display :'flex', justifyContent:'flex-end'  }  }   >
+            <p className='f3 link dim black underline pa3 pointer' > Sign Out</p>
+        </nav>
+    )
+}
+export default navigation;
