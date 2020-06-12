ded1335b887c7cb4b90e017a799383ef4d2396c1
prasoonjain006 <prasoonjain006@gmail.com>
2020-06-12 21:29:06 +0530

    change logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
-import logo1 from './logo1.png';
+import logo1 from './logo3.jfif';
 
 const Logo=()=>{
     return(
         <div className="ml2 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 35 ,scale:1.2}} style={{ height: 110, width: 110 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 35 ,scale:1.2}} style={{ height: 118, width: 120 }} >
                 <div className="Tilt-inner">
-                    <img className="pa3"  src={logo1}  alt='logo'/>
+                    <img className="pa2"  src={logo1} style={{ height: 100, width: 100 }} alt='logo'/>
                 </div>
             </Tilt>
         </div>
